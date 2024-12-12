5c37733895b28d6a3d837f326ff07cabceaa0fd4
Ahmed Rabie <110050965+ahmed-rabi@users.noreply.github.com>
2024-12-12 20:03:36 +0200

    Add files via upload

--- Hash.cpp
+++ Hash.cpp
@@ -2,28 +2,42 @@
 using namespace std;
 
 // (a) Division Method
-int divisionHash(int key, int tableSize) {
+int divisionHash(int key, int tableSize)
+{
     return key % tableSize;
 }
 
 // (b) Multiplication Method
-int multiplicationHash(int key, int tableSize) {
+int multiplicationHash(int key, int tableSize)
+{
     double A = 0.555; // A constant between 0 and 1
     double fractionalPart = key * A - int(key * A);
     return int(tableSize * fractionalPart);
 }
 
 // (c) Mid Square Method
-int midSquareHash(int key, int tableSize) {
-    int squared = key * key;
-    int mid = (squared / 100) % 100; // Extracting the middle two digits
+int midSquareHash(int key, int tableSize)
+{
+    int digits = to_string(tableSize - 1).length();
+    string str = to_string(key * key); // 6  123456
+    int mid = 0;
+    if (str.length() >= digits)
+    {
+        mid = stoi(str.substr(str.length() / 2 - digits / 2, digits));
+    }
+    else
+    {
+        mid = key * key;
+    }
     return mid % tableSize;
 }
 
 // (d) Folding Method
-int foldingHash(int key, int tableSize) {
+int foldingHash(int key, int tableSize)
+{
     int sum = 0;
-    while (key > 0) {
+    while (key > 0)
+    {
         sum += key % 100; // Extract last two digits
         key /= 100;
     }
@@ -31,30 +45,38 @@ int foldingHash(int key, int tableSize) {
 }
 
 // (a) Chaining Method
-class ChainingHashTable {
+class ChainingHashTable
+{
     vector<list<int>> table;
     int tableSize;
 
 public:
     ChainingHashTable(int size) : tableSize(size), table(size) {}
 
-    void insert(int key) {
+    void insert(int key)
+    {
         int index = divisionHash(key, tableSize);
         table[index].push_back(key);
     }
 
-    bool search(int key) {
+    bool search(int key)
+    {
         int index = divisionHash(key, tableSize);
-        for (int item : table[index]) {
-            if (item == key) return true;
+        for (int item : table[index])
+        {
+            if (item == key)
+                return true;
         }
         return false;
     }
 
-    bool remove(int key) {
+    bool remove(int key)
+    {
         int index = divisionHash(key, tableSize);
-        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
-            if (*it == key) {
+        for (auto it = table[index].begin(); it != table[index].end(); ++it)
+        {
+            if (*it == key)
+            {
                 table[index].erase(it);
                 return true;
             }
@@ -64,119 +86,146 @@ public:
 };
 
 // (b) Open Addressing Method
-class OpenAddressingHashTable {
+class OpenAddressingHashTable
+{
     vector<int> table;
     int tableSize;
-    const int DELETED = -2;  // Mark for deleted elements
+    const int DELETED = -2; // Mark for deleted elements
 
 public:
     OpenAddressingHashTable(int size) : tableSize(size), table(size, -1) {}
 
-    void insert(int key) {
+    void insert(int key)
+    {
         int index = divisionHash(key, tableSize);
-        while (table[index] != -1 && table[index] != DELETED) { // Linear probing
+        while (table[index] != -1 && table[index] != DELETED)
+        { // Linear probing
             index = (index + 1) % tableSize;
         }
         table[index] = key;
     }
 
-    bool search(int key) {
+    bool search(int key)
+    {
         int index = divisionHash(key, tableSize);
         int startIndex = index;
-        while (table[index] != -1) {
-            if (table[index] == key) return true;
+        while (table[index] != -1)
+        {
+            if (table[index] == key)
+                return true;
             index = (index + 1) % tableSize;
-            if (index == startIndex) break; // Loop detected
+            if (index == startIndex)
+                break; // Loop detected
         }
         return false;
     }
 
-    bool remove(int key) {
+    bool remove(int key)
+    {
         int index = divisionHash(key, tableSize);
         int startIndex = index;
-        
-        while (table[index] != -1) {
-            if (table[index] == key) {
-                table[index] = DELETED;  // Mark as deleted
+
+        while (table[index] != -1)
+        {
+            if (table[index] == key)
+            {
+                table[index] = DELETED; // Mark as deleted
                 return true;
             }
             index = (index + 1) % tableSize;
-            if (index == startIndex) break; // Loop detected
+            if (index == startIndex)
+                break; // Loop detected
         }
         return false;
     }
 };
 
 // (c) Double Hashing
-class DoubleHashingHashTable {
+class DoubleHashingHashTable
+{
     vector<int> table;
     int tableSize;
-    const int DELETED = -2;  // Mark for deleted elements
+    const int DELETED = -2; // Mark for deleted elements
 
-    int hash2(int key) {
+    int hash2(int key)
+    {
         return 1 + (key % (tableSize - 1));
     }
 
 public:
     DoubleHashingHashTable(int size) : tableSize(size), table(size, -1) {}
 
-    void insert(int key) {
+    void insert(int key)
+    {
         int index = divisionHash(key, tableSize);
-        int step = hash2(key);
+        int step = multiplicationHash(key, tableSize);
 
-        while (table[index] != -1 && table[index] != DELETED) {
+        while (table[index] != -1 && table[index] != DELETED)
+        {
             index = (index + step) % tableSize;
         }
         table[index] = key;
     }
 
-    bool search(int key) {
+    bool search(int key)
+    {
         int index = divisionHash(key, tableSize);
-        int step = hash2(key);
+        int step = multiplicationHash(key, tableSize);
         int startIndex = index;
 
-        while (table[index] != -1) {
-            if (table[index] == key) return true;
+        while (table[index] != -1)
+        {
+            if (table[index] == key)
+                return true;
             index = (index + step) % tableSize;
-            if (index == startIndex) break; // Loop detected
+            if (index == startIndex)
+                break; // Loop detected
         }
         return false;
     }
 
-    bool remove(int key) {
+    bool remove(int key)
+    {
         int index = divisionHash(key, tableSize);
-        int step = hash2(key);
+        int step = multiplicationHash(key, tableSize);
         int startIndex = index;
 
-        while (table[index] != -1) {
-            if (table[index] == key) {
-                table[index] = DELETED;  // Mark as deleted
+        while (table[index] != -1)
+        {
+            if (table[index] == key)
+            {
+                table[index] = DELETED; // Mark as deleted
                 return true;
             }
             index = (index + step) % tableSize;
-            if (index == startIndex) break; // Loop detected
+            if (index == startIndex)
+                break; // Loop detected
         }
         return false;
     }
 };
 
 // (d) Quadratic Probing
-class QuadraticProbingHashTable {
+class QuadraticProbingHashTable
+{
     vector<int> table;
     int tableSize;
-    const int DELETED = -2;  // Mark for deleted elements
+    const int DELETED = -2; // Mark for deleted elements
 
 public:
     QuadraticProbingHashTable(int size) : tableSize(size), table(size, -1) {}
 
-    void insert(int key) {
+    void insert(int key)
+    {
         int index = divisionHash(key, tableSize);
-        int i = 0;  // Quadratic probing increment
-        
-        while (table[index] != -1 && table[index] != DELETED) {
+        int i = 0; // Quadratic probing increment
+
+        while (table[index] != -1 && table[index] != DELETED)
+        {
             i++;
-            index = (divisionHash(key, tableSize) + i * i) % tableSize;  // Quadratic probing
-            if (i >= tableSize) {  // If we've tried all possible positions
+            index = (divisionHash(key, tableSize) + i * i) % tableSize; // Quadratic probing
+            if (i >= tableSize)
+            { // If we've tried all possible positions
                 cout << "Table is full or no valid position found for " << key << "\n";
                 return;
             }
@@ -184,56 +233,70 @@ public:
         table[index] = key;
     }
 
-    bool search(int key) {
+    bool search(int key)
+    {
         int index = divisionHash(key, tableSize);
         int i = 0;
         int startIndex = index;
-        
-        do {
-            if (table[index] == key) return true;
-            if (table[index] == -1) return false;  // Empty slot means key not found
-            
+
+        do
+        {
+            if (table[index] == key)
+                return true;
+            if (table[index] == -1)
+                return false; // Empty slot means key not found
+
             i++;
             index = (divisionHash(key, tableSize) + i * i) % tableSize;
         } while (index != startIndex && i < tableSize);
-        
+
         return false;
     }
 
-    bool remove(int key) {
+    bool remove(int key)
+    {
         int index = divisionHash(key, tableSize);
         int i = 0;
         int startIndex = index;
-        
-        do {
-            if (table[index] == key) {
+
+        do
+        {
+            if (table[index] == key)
+            {
                 table[index] = DELETED;
                 return true;
             }
-            if (table[index] == -1) return false;  // Empty slot means key not found
-            
+            if (table[index] == -1)
+                return false; // Empty slot means key not found
+
             i++;
             index = (divisionHash(key, tableSize) + i * i) % tableSize;
         } while (index != startIndex && i < tableSize);
-        
+
         return false;
     }
 
-    void display() {
+    void display()
+    {
         cout << "Hash Table Contents:\n";
-        for (int i = 0; i < tableSize; i++) {
+        for (int i = 0; i < tableSize; i++)
+        {
             cout << i << ": ";
-            if (table[i] == -1) cout << "Empty";
-            else if (table[i] == DELETED) cout << "Deleted";
-            else cout << table[i];
+            if (table[i] == -1)
+                cout << "Empty";
+            else if (table[i] == DELETED)
+                cout << "Deleted";
+            else
+                cout << table[i];
             cout << "\n";
         }
     }
 };
 
-int main() {
+int main()
+{
     int tableSize = 10;
-    
+
     // Test Chaining Hash Table
     ChainingHashTable chainingTable(tableSize);
     cout << "Testing Chaining Hash Table:\n";
